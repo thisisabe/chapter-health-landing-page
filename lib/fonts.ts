a881a70cd08f3beb79b702356ba3aded497040fd
thisisabe <abe@thinkermaker.com.au>
2025-08-21 23:40:01 +1000

    Update fonts.ts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,16 +1,2 @@
-import localFont from "next/font/local"
-
-export const heading = localFont({
-  src: [{ path: "../public/fonts/national-2-narrow-medium.woff2", weight: "600", style: "normal" }],
-  variable: "--font-heading",
-  display: "swap",
-})
-
-export const body = localFont({
-  src: [
-    { path: "../public/fonts/PPMori-Regular.woff2", weight: "400", style: "normal" },
-    { path: "../public/fonts/PPMori-SemiBold.woff2", weight: "600", style: "normal" },
-  ],
-  variable: "--font-body",
-  display: "swap",
-})
+export const heading = { variable: "--font-heading" } as any
+export const body = { variable: "--font-body" } as any
