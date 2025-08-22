f061f74ebf2d14476aa1240daf3758feb53128ec
thisisabe <abe@thinkermaker.com.au>
2025-08-22 17:52:07 +1000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = { content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"], theme: { extend: { colors: { "health-core":"#18051E", "light-balance":"#E8CDFF", "restorative-yellow":"#FBF999", "peak-performance":"#FF482C" }, fontFamily:{ heading:["var(--font-heading)"], body:["var(--font-body)"] } } }, plugins: [] }
